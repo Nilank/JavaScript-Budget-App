b2028547c86f9e4d25306970dc8d652d28096334
Nilank <sharmanilank.com>
2019-07-15 21:12:37 -0400

    Add Code in UIController

--- app.js
+++ app.js
@@ -2,38 +2,47 @@
 //Budget Controller
 var budgetController = (function(){
 
-
 })();
 
 //User Interface Controller
 var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
 
-    //We'll write code later
+    return {
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMStrings: function () {
+            return DOMstrings;
 
-
+        }
+    }
 })();
 
 //Global App Controller
 var controller = (function(bdgtCntrl, UICtrl){
 
-    var ctrlAddItem  = function(){
+    var DOM = UICtrl.getDOMStrings();
 
-        alert("It Worked!");
-
+     var ctrlAddItem  = function(){
+         var input = UIController.getInput();
+         console.log(input);
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if(event.keyCode === 13){
             ctrlAddItem();
         }
     });
-
-
-
-
-
-
-
 })(budgetController, UIController);
