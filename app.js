5616b045b61ce7a4082bd532dfa7cf229d920c0b
Nilank <sharmanilank.com>
2019-07-17 16:31:56 -0400

    Undefined bug fixed in the UIController

--- app.js
+++ app.js
@@ -76,28 +76,17 @@ var UIController = (function(){
             };
         },
         addListItem: function(obj, type){
-            var html, newHtml;
+            var html, newHtml, element;
 
             // Create HTML string using placeholder text
             if(type === 'inc'){
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div> \n' +
-                    '                <div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"> \n' +
-                    '                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>\n' +
-                    '            </div>'
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }else if(type === 'exp'){
-                html ='<div class="item clearfix" id="expense-%id%">\n' +
-                    '                    <div class="item__description">%description%</div>\n' +
-                    '                    <div class="right clearfix">\n' +
-                    '                        <div class="item__value">%value%</div>\n' +
-                    '                        <div class="item__percentage">21%</div>\n' +
-                    '                        <div class="item__delete">\n' +
-                    '                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>\n' +
-                    '                        </div>\n' +
-                    '                    </div>\n' +
-                    '                </div>'
+                element = DOMstrings.expensesContainer;
+                html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
-
-
+            
             // Replace the placeholder test with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
@@ -105,6 +94,7 @@ var UIController = (function(){
 
 
             // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
 
         },
@@ -136,6 +126,9 @@ var controller = (function(bdgtCntrl, UICtrl){
 
          //Add the item to the budget controller
          var newItem =budgetController.addItem(input.type, input.description, input.value);
+
+         //Add the item to the UI
+         UICtrl.addListItem(newItem, input.type);
      };
 
      return {
