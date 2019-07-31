4fad1413f4f0cfe0fd3cd69397433be5e59d2860
Nilank <sharmanilank.com>
2019-07-31 15:13:12 -0400

    UI Improvements

--- app.js
+++ app.js
@@ -179,6 +179,12 @@ var UIController = (function(){
         return (type === 'exp' ? '-'  : '+') + ' '+ int + '.' +  dec;
     };
 
+    var nodeListForEach = function(list,callback){
+        for(var i=0; i<list.length; i++){
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function(){
             return{
@@ -246,12 +252,6 @@ var UIController = (function(){
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLable);
 
-            var nodeListForEach = function(list,callback){
-                for(var i=0; i<list.length; i++){
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index){
 
                 if(percentages[index] > 0){
@@ -272,6 +272,23 @@ var UIController = (function(){
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
+        changedType: function(){
+
+            var fields = document.querySelectorAll(DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function (cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputButton).classList.toggle('red');
+
+
 
         },
 
@@ -296,6 +313,8 @@ var controller = (function(bdgtCntrl, UICtrl){
         });
 
         document.querySelector((DOM.container)).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
     
     var updateBudget = function () {
